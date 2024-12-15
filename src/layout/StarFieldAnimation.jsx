fd1a5fb0f0c6099c737f98d698178521de230af6
ujjwaltamboli <ujjwaltamboli1@gmail.com>
2024-12-15 12:51:45 +0530

    team page changes

---
+++ src/layout/StarFieldAnimation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './StarFieldAnimation.css'; // Import your CSS file
+
+function StarFieldAnimation() {
+  return (
+    <div className="main2" style={{position:"relative", zIndex:"0"}}>
+    </div>
+  );
+}
+
+export default StarFieldAnimation;
